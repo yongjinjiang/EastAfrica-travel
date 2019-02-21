bd393aedee3f0cbccc75413473970ea3416c1d1c
jyj <yjjiangphysics@gmail.com>
2019-02-21 10:00:33 -0600

    set route /table to render list.html to show all registered people: not working yet

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ from flask import Flask, jsonify, render_template,flash,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 
 from forms import LoginForm
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'any secret string'
+
+app.config["TEMPLATES_AUTO_RELOAD"]=True
+
 
 @app.route("/")
 def index():
@@ -40,13 +44,13 @@ csv_path="./registered_names.csv"
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    print()
+
     print('form.validate_on_submit()=',form.validate_on_submit())
 
     if form.validate_on_submit():
         flash('Login requested for user {}, {}'.format(
             form.first_name.data, form.last_name.data))
-        print('flash data',form.first_name.data)
+        # print('flash data',form.first_name.data)
         person=dict()
         person["first_name"]=form.first_name.data
         person["last_name"]=form.last_name.data
@@ -63,7 +67,7 @@ def login():
        
     
         return render_template('login.html',form=form)
-    print("hhheeeeehhehehhehehe")
+    
     return render_template('login.html',  form=form)
 
 @app.route("/registered_people")
@@ -74,9 +78,15 @@ def names():
 
     for index, row in people_df.iterrows():
           
-          people.append({"first name": row["first_name"],"last name": row["last_name"]})
-    print("people=", people)
+          people.append({"first_name": row["first_name"],"last_name": row["last_name"]})
+    # print("people=", people)
     return   jsonify(people)
+
+@app.route("/table")
+def list():
+    return render_template('list.html')
+
+
 
 
 if __name__ == "__main__":
--- static/js/registered_names.js
+++ static/js/registered_names.js
@@ -1,2 +1,59 @@
-d3.select("#registered_people").text("here!!!!!!")
 
+console.log('I am here!')
+
+
+
+d3.select("#registered_list").append("div")
+.attr("class","col-xs-12  col-md-12")  
+.append("div")
+.attr('id',"people_list")
+
+// url={{ url_for('static',filename='./registered_names.csv')}}
+
+d3.csv("./registered_names.csv", function(error, tvData) {
+   if (error) return console.warn(error);
+ 
+   console.log(tvData);
+}
+)
+
+
+
+
+// d3.json(`/registered_people`,function(error, data) {
+
+//    if (error) return console.warn(error);
+
+//    console.log("data",data)
+//    console.log("12345567")
+//   console.log("first name",data["first_name"]) 
+//   console.log("last name",data["last_name"]) 
+
+// })
+//  var table = d3.select("#people_list")
+//     .append('div').attr("class","col-md-12").append('div')
+//      .attr("id","table-area")
+//      .style("text-align", "center")
+//      .append('table')
+//      .attr('class','table-striped')
+//      .style("width","100%")
+
+// var thead=table.append('thead')
+
+// thead.append('th')
+// .style("min-width","90px")
+// .text("first name ");
+
+// thead.append('th')
+// .style("min-width","190px")
+// .text("last name");
+
+// tbody=table.append('tbody');
+// row=tbody.append('tr')
+// row.append('td').text(person.first_name)
+// row.append('td').text(person.first_name)
+
+    
+// }
+
+// )
