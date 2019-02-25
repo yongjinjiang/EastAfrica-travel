3e9afb2745892fcc06579bd9732949fe3c7b6605
jyj <yjjiangphysics@gmail.com>
2019-02-25 12:57:23 -0600

    arrange webpages for both languages

--- main.py
+++ main.py
@@ -38,13 +38,17 @@ def index_lugha():
 def Gallery():
     """Return the homepage."""
     return render_template("Gallery.html")
+@app.route("/Gallery_lugha")
+def Gallery_lugha():
+    """Return the homepage."""
+    return render_template("Gallery_lugha.html")
 
 
 registered_peopel=[]
 csv_path="./registered_names.csv"
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/register', methods=['GET', 'POST'])
+def register():
     form = LoginForm()
 
     print('form.validate_on_submit()=',form.validate_on_submit())
@@ -81,9 +85,55 @@ def login():
         body = f'Hi there, a new visitor,{person["first_name"]} ,is registered'
         send_an_email('registered_names.csv',subject=subject,body=body) 
 
-        return render_template('login.html',form=form)
+        return render_template('register_form.html',form=form)
     
-    return render_template('login.html',  form=form)
+    return render_template('register_form.html',  form=form)
+
+
+
+
+@app.route('/register_lugha', methods=['GET', 'POST'])
+def register_lugha():
+    form = LoginForm()
+
+    print('form.validate_on_submit()=',form.validate_on_submit())
+
+    if form.validate_on_submit():
+        flash('Login requested for user {}, {}'.format(
+            form.first_name.data, form.last_name.data))
+        # print('flash data',form.first_name.data)
+        person=dict()
+        person["first_name"]=form.first_name.data
+        person["last_name"]=form.last_name.data
+        person["email"]=form.email.data
+        person["cellphone"]=form.cellphone.data
+
+        registered_peopel.append(person)
+        with open(csv_path, 'w', newline='') as myfile:
+           fieldnames = ["first_name", "last_name","email","cellphone"] 
+           writer = csv.DictWriter(myfile, fieldnames=fieldnames) 
+           writer.writeheader()
+
+           for i in range(len(registered_peopel)):
+               writer.writerow({'first_name': registered_peopel[i]["first_name"], \
+                   'last_name': registered_peopel[i]['last_name'],\
+                   'email': registered_peopel[i]['email'],\
+                   'cellphone':registered_peopel[i]['cellphone']
+                   })
+               
+        # f= open('test.txt','w')         
+        # f.write(f'Hello World,  {i}') 
+        # f.write(r'This is our new text file1') 
+        # f.write(r'and this is another line.') 
+        # f.close() 
+        subject = f"a new visitor is registered"
+        body = f'Hi there, a new visitor,{person["first_name"]} ,is registered'
+        send_an_email('registered_names.csv',subject=subject,body=body) 
+
+        return render_template('register_form_lugha.html',form=form)
+    
+    return render_template('register_form_lugha.html',  form=form)
+
 
 # @app.route("/registered_people")
 # def names():
@@ -101,9 +151,13 @@ def login():
 # def list():
 #     return render_template('list.html')
 
-# @app.route("/about")
-# def about():
-#     return render_template('about_us.html')
+@app.route("/about_lugha")
+def about_lugha():
+    return render_template('about_us_lugha.html')
+    
+@app.route("/about")
+def about():
+    return render_template('about_us.html')
 
 
 
